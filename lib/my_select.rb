386aeba379db59672ce2446b573b8a349c6a6097
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-04 16:53:01 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ counter = 0
+collection2 = []
+while counter<collection.length
+  element = collection[counter]
+  collection2 << yield(element)
+  #
+  counter+=1
+end
+collection2
 end
